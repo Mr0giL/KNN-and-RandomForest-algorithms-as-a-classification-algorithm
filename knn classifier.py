3ac684b34fa4873928a838d2da0dab00ea692286
Mohammad Javad Dadashi <112317271+Mr0giL@users.noreply.github.com>
2022-10-23 23:37:00 +0330

    Add files via upload
    the code is written in python and prediction and score of each algorithm is here. We use Iris dataset in a csv format to run these algorithms. this dataset is a good training dataset for running claccification algorithms on it

---
+++ knn classifier.py
@@ -0,0 +1,56 @@
+# import needed libraries
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as mtp
+from sklearn.model_selection import train_test_split as tts
+from sklearn.neighbors import KNeighborsClassifier as knn
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import precision_score
+############################################
+df = pd.read_csv('iris_csv.csv') # load the Iris-dataset
+# print(df.info()) # show some basic informations about dataset
+############################################
+# seperate target column as y to other columns
+x = df.drop('class',axis=1,inplace=False)
+y= df['class']
+############################################
+x_train, x_test, y_train, y_test = tts(x,y,test_size=0.3,random_state=0) # create random train and test variables from x and y
+############################################
+ns = knn(n_neighbors=3) # call Knn.- we have 3 class so n_neighbors is 3
+ns.fit(x_train,y_train) # use x_train data to learn algorithm
+pred = ns.predict(x_test) # predict the x_test algorithm
+pred_results = pd.DataFrame({'expected_y':y_test ,'predicted_y':pred}) # save the y_test and predicts in a csv file
+########################################
+# how to run scores
+y_true = y_test # create new variable for y_test to use in scores
+y_pred = pred # create new variable for predicted values to use in scores
+
+acc_sc =  (accuracy_score(y_true,y_pred)).astype(str) # call and save accuarcy of real test targets and predictions score in a variable
+conf_mat = (confusion_matrix(y_true,y_pred)) # call and save confusion matrix of real test targets and predictions in a variable
+jac_sc = jaccard_score(y_true,y_pred, labels = ['Iris-setosa','Iris-versicolor','Iris-virginica'] , average='macro').astype(str) # call and save Iou of real test targets and predictions score in a variable
+prec_sc = (precision_score(y_true,y_pred,labels = ['Iris-setosa','Iris-versicolor','Iris-virginica'] , average='macro')).astype(str) # call and save persicion of real test targets and predictions score in a variable
+
+#print scores
+print('accuracy score is : ' , acc_sc)
+print('confusion matrix is : ', conf_mat )
+print('IoU_score is : ', jac_sc)
+print('precision score is : ' , prec_sc)
+
+f = open('knn_accuracy score.txt','w') # to open or create a txt file
+f.write(acc_sc) # write a variable in the opened file
+f.close() # to stop writing on the opened file
+
+np.savetxt('knn_confusion matrix.txt',conf_mat, fmt = '%10.5f') # save confusion matrix 
+
+f = open('knn_IoU score.txt','w')
+f.write(jac_sc)
+f.close()
+
+f = open('knn_precision score.txt','w')
+f.write(prec_sc)
+f.close()
+
+pred_results.to_csv('knn_results.csv') # create a csv file for real y test data and prediction of it
